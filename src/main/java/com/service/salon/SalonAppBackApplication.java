967f659d7cc9ef988c25c5e2ffe0704b0cd07d71
dmi6756@yandex.ru <a0zvx753>
2026-05-12 15:28:28 +0300

    first commit

---
+++ src/main/java/com/service/salon/SalonAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.service.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/salon/SalonAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
